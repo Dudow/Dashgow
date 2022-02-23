6a68486b2bff0886d1a435b20f21255ae007e95a
Dudow <edu30.nm@gmail.com>
2022-02-23 10:52:26 -0300

    feat: add SignIn Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,54 @@
+import { Flex, Input, Button, FormLabel, FormControl } from "@chakra-ui/react";
+
 export default function Home() {
-  return <h1>Deu tudo certo</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+        gridGap={4}
+      >
+        <FormControl>
+          <FormLabel htmlFor="email">E-mail</FormLabel>
+          <Input
+            name="email"
+            id="email"
+            type="email"
+            focusBorderColor="pink.500"
+            bgColor="gray.900"
+            variant="filled"
+            size="lg"
+            _hover={{
+              bgColor: "gray.900",
+            }}
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input
+            name="password"
+            id="password"
+            type="password"
+            focusBorderColor="pink.500"
+            bgColor="gray.900"
+            variant="filled"
+            size="lg"
+            _hover={{
+              bgColor: "gray.900",
+            }}
+          />
+        </FormControl>
+
+        <Button type="submit" mt="2" colorScheme="pink">
+          Submit
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
